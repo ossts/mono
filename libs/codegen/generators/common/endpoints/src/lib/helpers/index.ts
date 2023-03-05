cead5e3faaace101d24aebd264d1291adf08ef0c
Volodymyr <vpanov.me@gmail.com>
2023-03-05 20:09:56 +0200

    feat(codegen): update generators with helpers boilerplate. Add targets for templates precompilation

---
+++ libs/codegen/generators/common/endpoints/src/lib/helpers/index.ts
@@ -0,0 +1,10 @@
+import type { GeneratorHelpersExportType } from '@ossts/codegen/common';
+import type { DictionaryWithAny } from '@ossts/shared/typescript/helper-types';
+
+// import * as localHelpers from './local';
+// import * as globalHelpers from './global';
+
+export const helpers: Record<GeneratorHelpersExportType, DictionaryWithAny> = {
+  localHelpers: {},
+  globalHelpers: {},
+};
--- libs/codegen/generators/common/models/src/index.ts
+++ libs/codegen/generators/common/models/src/index.ts
@@ -1,8 +1,4 @@
-// export * from './lib';
+export * from './lib';
 export * from './lib/typings';
-export * from './lib/generator';
 
-// this is excluded from git history
-// execute the following to generate this export
-// nx run codegen-generators-runtime:precompile-templates
 export * from './lib/precompiled-templates';
