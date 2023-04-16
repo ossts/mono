d1ec0c7c77262f14883887b1e231e10be3be98ab
Volodymyr <vpanov.me@gmail.com>
2023-04-16 14:46:57 +0300

    feat(workspace): update NX

--- apps/tooling/plugin-codegen-e2e/jest.config.ts
+++ apps/tooling/plugin-codegen-e2e/jest.config.ts
@@ -2,13 +2,14 @@
 export default {
   displayName: 'tooling-plugin-codegen-e2e',
   preset: '../../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
+  globals: {},
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../../coverage/apps/tooling/plugin-codegen-e2e',
