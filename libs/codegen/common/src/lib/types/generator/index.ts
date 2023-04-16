85e95cd03fc65476b24ef0092cf050eb9ab1edb2
Volodymyr <vpanov.me@gmail.com>
2023-04-16 19:47:35 +0300

    ci(codegen): fix CI build

--- libs/codegen/common/src/lib/types/generator/index.ts
+++ libs/codegen/common/src/lib/types/generator/index.ts
@@ -1,12 +1,13 @@
-import type { AbstractExternalGeneratorSettings } from './AbstractExternalGenerator';
+import type { AbstractGeneratorSettings } from './AbstractGenerator';
 import { AbstractGenerator } from './AbstractGenerator';
 
-// export * from './AbstractExternalGenerator';
 export * from './AbstractExternalGeneratorUxname';
 export * from './AbstractGenerate';
 export * from './AbstractGenerator';
 export * from './GeneratorExportTypes';
 export * from './other';
+
+export type AbstractExternalGeneratorSettings = AbstractGeneratorSettings;
 
 export abstract class AbstractExternalGenerator extends AbstractGenerator {
   override settings?: AbstractExternalGeneratorSettings;
